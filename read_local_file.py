21b7146437b5be2f5b49a9b20c6f2cdeb5ea2011
irlan <irlangrangel2003@gmail.com>
2017-10-25 16:22:05 +0200

    Query local file

--- read_local_file.py
+++ read_local_file.py
@@ -4,7 +4,16 @@ import pprint
 g = Graph()
 g.parse("sto.ttl", format="turtle")
 
-print("--- start: turtle ---")
-print(g.serialize(format="turtle"))
-print("--- end: turtle ---\n")
+query = """
+    PREFIX sto: <https://w3id.org/i40/sto#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    
+    SELECT ?sub ?pred ?obj WHERE {
+         ?sub rdf:type sto:Standard
+    }
+    """
 
+#print query
+
+for row in g.query(query):
+    print "Predicate:%s Object:%s"%(row[0],row[1])
